1a3b99223fcd13dda6e6c48ab0c58cd660e62fac
benignstar <che0009@naver.com>
2017-03-28 20:23:23 +0900

    #2 Java Event Programing

--- app/src/main/java/benignstar/kr/hs/emirim/simplecalculator/MainActivity.java
+++ app/src/main/java/benignstar/kr/hs/emirim/simplecalculator/MainActivity.java
@@ -7,10 +7,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import static benignstar.kr.hs.emirim.simplecalculator.R.id.but_add;
+
+public class MainActivity extends AppCompatActivity{
     TextView textResult;
-    int num_first;
-    int num_second;
     EditText edit_first;
     EditText edit_second;
     @Override
@@ -25,13 +25,17 @@ public class MainActivity extends AppCompatActivity {
         Button but_sub=(Button) findViewById(R.id.but_sub);
         Button but_mul=(Button) findViewById(R.id.but_mul);
         Button but_div=(Button) findViewById(R.id.but_div);
+        but_add.setOnClickListener(butHandler);
+        but_sub.setOnClickListener(butHandler);
+        but_mul.setOnClickListener(butHandler);
+        but_div.setOnClickListener(butHandler);
 
-        but_add.setOnClickListener(new View.OnClickListener(){
+        /*but_add.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View view) {
-                num_first=Integer.parseInt(edit_first.getText().toString());
-                num_second=Integer.parseInt(edit_second.getText().toString());
+                int num_first=Integer.parseInt(edit_first.getText().toString());
+                int num_second=Integer.parseInt(edit_second.getText().toString());
                 textResult.setText(String.valueOf("실행결과 : "+(num_first+num_second)));
             }
         });
@@ -40,8 +44,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                num_first=Integer.parseInt(edit_first.getText().toString());
-                num_second=Integer.parseInt(edit_second.getText().toString());
+                int num_first=Integer.parseInt(edit_first.getText().toString());
+                int num_second=Integer.parseInt(edit_second.getText().toString());
                 textResult.setText(String.valueOf("실행결과 : "+(num_first-num_second)));
 
             }
@@ -51,8 +55,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                num_first=Integer.parseInt(edit_first.getText().toString());
-                num_second=Integer.parseInt(edit_second.getText().toString());
+                int num_first=Integer.parseInt(edit_first.getText().toString());
+                int num_second=Integer.parseInt(edit_second.getText().toString());
                 textResult.setText(String.valueOf("실행결과 : "+(num_first*num_second)));
             }
         });
@@ -61,10 +65,62 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                num_first=Integer.parseInt(edit_first.getText().toString());
-                num_second=Integer.parseInt(edit_second.getText().toString());
+                int num_first=Integer.parseInt(edit_first.getText().toString());
+                int num_second=Integer.parseInt(edit_second.getText().toString());
                 textResult.setText(String.valueOf("실행결과 : "+(num_first/num_second)));
             }
-        });
+        });*/
     }
+
+    /*@Override
+    public void onClick(View view) {
+        int num_first=Integer.parseInt(edit_first.getText().toString());
+        int num_second=Integer.parseInt(edit_second.getText().toString());
+        int result=0;
+
+        switch (view.getId()){
+            case but_add :
+                result=num_first+num_second;
+                break;
+            case R.id.but_sub :
+                result=num_first-num_second;
+                break;
+            case R.id.but_mul :
+                result=num_first*num_second;
+                break;
+            case R.id.but_div :
+                result=num_first/num_second;
+                break;
+        }
+
+        textResult.setText("실행결과 : "+(num_first+num_second));
+    }*/
+    View.OnClickListener butHandler=new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            int num_first = Integer.parseInt(edit_first.getText().toString());
+            int num_second = Integer.parseInt(edit_second.getText().toString());
+            int result = 0;
+
+            switch (view.getId()) {
+                case but_add:
+                    result = num_first + num_second;
+                    break;
+                case R.id.but_sub:
+                    result = num_first - num_second;
+                    break;
+                case R.id.but_mul:
+                    result = num_first * num_second;
+                    break;
+                case R.id.but_div:
+                    result = num_first / num_second;
+                    break;
+            }
+
+            textResult.setText("실행결과 : " + result);
+        }
+    };
+
+
 }
